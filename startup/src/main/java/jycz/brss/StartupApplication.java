61a67a9e5f05142dc34a46f84cb718c54c31bbf0
qianaiwuyu@gmail.com <qianaiwuyu@gmail.com>
2020-04-04 16:40:57 +0800

    为我的小可爱添加启动类

---
+++ startup/src/main/java/jycz/brss/StartupApplication.java
@@ -0,0 +1,15 @@
+package jycz.brss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ling
+ * @data 2020/4/4 16:37
+ */
+@SpringBootApplication
+public class StartupApplication {
+    public static void main(String[] args){
+        SpringApplication.run(StartupApplication.class, args);
+    }
+}
