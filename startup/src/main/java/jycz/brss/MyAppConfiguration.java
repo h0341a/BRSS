92cb63b44a48fcb2f11af0a49361db907fc5e93c
qianaiwuyu@gmail.com <qianaiwuyu@gmail.com>
2020-04-04 17:08:10 +0800

    添加了两个未雨绸缪的配置:支持跨域请求和使用swagger2自动生成接口文档,但这两个配置都只会在开发环境中生效。 在跨域请求中，第一次采用了CrosFilter的方式，它的优先级比较高。 关于swagger2没有什么多说的，方便测试,访问localhost:{port}/swagger-ui.html即可看到自己创建的api接口

---
+++ startup/src/main/java/jycz/brss/MyAppConfiguration.java
@@ -0,0 +1,48 @@
+package jycz.brss;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author ling
+ * @data 2020/4/4 16:53
+ */
+@Profile("dev")
+@Configuration
+@EnableSwagger2
+public class MyAppConfiguration {
+
+    /**
+     * 通过过滤器添加跨域支持,CorsFilter
+     * @return 一个配置好的CorsFilter
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
