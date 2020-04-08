89d288869af065e1d3d3b9ef2acc0ce15fc2bce8
qianaiwuyu@gmail.com <qianaiwuyu@gmail.com>
2020-04-08 14:47:04 +0800

    声明用户登录注册接口,只是为了使swagger产生文档方便测试，不会实现，后续会通过springsecurity实现

---
+++ common/src/main/java/com/jycz/common/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.jycz.common.controller;
+
+import com.jycz.common.response.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ling
+ * @data 2020/4/8 14:42
+ */
+@Api(tags = "LoginController")
+@RestController
+public class LoginController {
+    @PostMapping("/login")
+    public void login(){
+
+    }
+    @GetMapping("/logout")
+    public void logout(){
+
+    }
+}
