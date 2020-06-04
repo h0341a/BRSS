ec67d5174d98e32000266f84419a0fa2b6061ffd
h0341a <qianaiwuyu@gmail.com>
2020-06-04 11:05:32 +0800

    添加star表

--- common/src/main/resources/schema.sql
+++ common/src/main/resources/schema.sql
@@ -1,107 +1,129 @@
-create table role(
-                     id integer auto_increment primary key ,
-                     role_name VARCHAR(16) NOT NULL,
-                     index (id)
+create table role
+(
+    id        integer auto_increment primary key,
+    role_name VARCHAR(16) NOT NULL,
+    index (id)
 );
-INSERT into role(role_name) VALUES ('ROLE_ADMIN');
-INSERT into role(role_name) VALUES ('ROLE_USER');
+INSERT into role(role_name)
+VALUES ('ROLE_ADMIN');
+INSERT into role(role_name)
+VALUES ('ROLE_USER');
 
-create table user(
-                     id integer auto_increment primary key ,
-                     username varchar(32) NOT NULL UNIQUE ,
-                     email varchar(32) not null unique ,
-                     nickname varchar(50) NOT NULL UNIQUE ,
-                     password varchar(128) NOT NULL,
-                     role_id integer NOT NULL  ,
-                     foreign key (role_id) references role(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                     index(username)
+create table user
+(
+    id       integer auto_increment primary key,
+    username varchar(32)  NOT NULL UNIQUE,
+    email    varchar(32)  not null unique,
+    nickname varchar(50)  NOT NULL UNIQUE,
+    password varchar(128) NOT NULL,
+    role_id  integer      NOT NULL,
+    foreign key (role_id) references role (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    index (username)
 );
-create table user_info(
-                          uid INTEGER NOT NULL ,
-                          bio VARCHAR(160),
-                          avatar_url VARCHAR(128) default 'https://www.gravatar.com/avatar/hash?d=mp',
-                          fans_number INTEGER DEFAULT 0,
-                          idol_number INTEGER DEFAULT 0,
-                          read_books INTEGER DEFAULT 0,
-                          unread_books INTEGER DEFAULT 0,
-                          register_date datetime not null default now(),
-                          FOREIGN KEY (uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                          INDEX (uid)
+create table user_info
+(
+    uid           INTEGER  NOT NULL,
+    bio           VARCHAR(160),
+    avatar_url    VARCHAR(128)      default 'https://www.gravatar.com/avatar/hash?d=mp',
+    fans_number   INTEGER           DEFAULT 0,
+    idol_number   INTEGER           DEFAULT 0,
+    read_books    INTEGER           DEFAULT 0,
+    unread_books  INTEGER           DEFAULT 0,
+    register_date datetime not null default now(),
+    FOREIGN KEY (uid) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    INDEX (uid)
 );
-create table relation_group(
-                               id integer auto_increment primary key ,
-                               group_name VARCHAR(16) NOT NULL UNIQUE,
-                               index (id)
+create table relation_group
+(
+    id         integer auto_increment primary key,
+    group_name VARCHAR(16) NOT NULL UNIQUE,
+    index (id)
 );
-create table user_group(
-                           id integer auto_increment primary key ,
-                           uid integer not null ,
-                           gid integer not null ,
-                           FOREIGN KEY (uid) REFERENCES user(id),
-                           index (uid)
+create table star
+(
+    uid         int                    not null,
+    rid         int                    not null,
+    create_date DATETIME default NOW() not null,
+    foreign key (uid) references user (id) on update cascade on delete cascade,
+    foreign key (uid) references user_recommend (id) on update cascade on delete cascade
 );
-create table user_relation(
-                              source_id integer not null ,
-                              target_id integer not null ,
-                              status tinyint(1) not null COMMENT '用户关系状态:0为拉黑，1为关注',
-                              ugid integer not null ,
-                              FOREIGN KEY (source_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                              FOREIGN KEY (target_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                              FOREIGN KEY (ugid) REFERENCES user_group(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                              index (source_id)
+
+create table user_group
+(
+    id  integer auto_increment primary key,
+    uid integer not null,
+    gid integer not null,
+    FOREIGN KEY (uid) REFERENCES user (id),
+    index (uid)
 );
-create table user_message(
-                             send_uid integer,
-                             accept_uid integer,
-                             content VARCHAR(320) not null ,
-                             send_date datetime not null default now(),
-                             send_status tinyint not null COMMENT '发送状态:0发送失败,1发送成功',
-                             read_status tinyint not null COMMENT '阅读状态:(0,1)->(已读,未读)',
-                             FOREIGN KEY (send_uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                             FOREIGN KEY (accept_uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                             index (accept_uid)
+create table user_relation
+(
+    source_id integer    not null,
+    target_id integer    not null,
+    status    tinyint(1) not null COMMENT '用户关系状态:0为拉黑，1为关注',
+    ugid      integer    not null,
+    FOREIGN KEY (source_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (target_id) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ugid) REFERENCES user_group (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    index (source_id)
 );
-create table book(
-                     id integer auto_increment primary key ,
-                     name VARCHAR(64) NOT NULL ,
-                     author VARCHAR(64) NOT NULL,
-                     introduction varchar(336) NOT NULL ,
-                     commit_date datetime default now(),
-                     commit_user integer not null,
-                     rank double default 0.00,
-                     status tinyint(1) not null default 0 comment '判断该书籍是否通过审核0未通过1通过',
-                     FOREIGN KEY (commit_user) REFERENCES user(id)
+create table user_message
+(
+    send_uid    integer,
+    accept_uid  integer,
+    content     VARCHAR(320) not null,
+    send_date   datetime     not null default now(),
+    send_status tinyint      not null COMMENT '发送状态:0发送失败,1发送成功',
+    read_status tinyint      not null COMMENT '阅读状态:(0,1)->(已读,未读)',
+    FOREIGN KEY (send_uid) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (accept_uid) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    index (accept_uid)
 );
-create table user_recommend(
-                               id integer auto_increment primary key ,
-                               title varchar(128),
-                               content text,
-                               stars INTEGER DEFAULT 0,
-                               recommend_date datetime default now(),
-                               status tinyint(1) not null default 0 comment '判断该推荐是否通过审核0未通过1通过',
-                               bid integer,
-                               uid integer,
-                               FOREIGN KEY (uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                               FOREIGN KEY (bid) REFERENCES book(id) ON DELETE CASCADE ON UPDATE CASCADE,
-                               index (uid)
+create table book
+(
+    id           integer auto_increment primary key,
+    name         VARCHAR(64)  NOT NULL,
+    author       VARCHAR(64)  NOT NULL,
+    introduction varchar(336) NOT NULL,
+    commit_date  datetime              default now(),
+    commit_user  integer      not null,
+    rank         double                default 0.00,
+    status       tinyint(1)   not null default 0 comment '判断该书籍是否通过审核0未通过1通过',
+    FOREIGN KEY (commit_user) REFERENCES user (id)
 );
-create table user_comment(
-                             id INTEGER PRIMARY KEY AUTO_INCREMENT,
-                             content TEXT NOT NULL,
-                             parent_id INTEGER DEFAULT 0,
-                             comment_date DATETIME NOT NULL DEFAULT NOW(),
-                             uid INTEGER NOT NULL ,
-                             rid INTEGER NOT NULL ,
-                             FOREIGN KEY (uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                             FOREIGN KEY (rid) REFERENCES user_recommend(id) ON DELETE CASCADE ON UPDATE CASCADE
+create table user_recommend
+(
+    id             integer auto_increment primary key,
+    title          varchar(128),
+    content        text,
+    stars          INTEGER             DEFAULT 0,
+    recommend_date datetime            default now(),
+    status         tinyint(1) not null default 0 comment '判断该推荐是否通过审核0未通过1通过',
+    bid            integer,
+    uid            integer,
+    FOREIGN KEY (uid) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (bid) REFERENCES book (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    index (uid)
 );
-create table user_collection(
-                                bid INTEGER NOT NULL ,
-                                uid INTEGER NOT NULL ,
-                                status INTEGER DEFAULT 0,
-                                collection_date DATETIME NOT NULL DEFAULT NOW(),
-                                FOREIGN KEY (uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE ,
-                                FOREIGN KEY (bid) REFERENCES book(id) ON DELETE CASCADE ON UPDATE CASCADE
+create table user_comment
+(
+    id           INTEGER PRIMARY KEY AUTO_INCREMENT,
+    content      TEXT     NOT NULL,
+    parent_id    INTEGER           DEFAULT 0,
+    comment_date DATETIME NOT NULL DEFAULT NOW(),
+    uid          INTEGER  NOT NULL,
+    rid          INTEGER  NOT NULL,
+    FOREIGN KEY (uid) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (rid) REFERENCES user_recommend (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+create table user_collection
+(
+    bid             INTEGER  NOT NULL,
+    uid             INTEGER  NOT NULL,
+    status          INTEGER           DEFAULT 0,
+    collection_date DATETIME NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (uid) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (bid) REFERENCES book (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
